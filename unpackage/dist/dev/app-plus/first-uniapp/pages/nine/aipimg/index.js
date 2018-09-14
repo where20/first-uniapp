04afb8469dd5737960d6c8401b2f9c10138220e1
傅伟安 <fuwa@stjf.com>
2018-09-14 18:15:58 +0800

    update

---
+++ unpackage/dist/dev/app-plus/first-uniapp/pages/nine/aipimg/index.js
@@ -0,0 +1,13 @@
+
+      !(function(){
+        var uniAppViewReadyCallback = function(){
+          setCssToHead([".",[1],"img_wrap { margin-bottom: ",[0,10],"; width: 100%; min-height: ",[0,300],"; background: #ececec; padding: ",[0,20]," ",[0,40],"; box-sizing: border-box; }\n.",[1],"img_wrap wx-image { width: 100%; }\n.",[1],"title { display: inline-block; width: 100%; font-size: ",[0,28],"; text-align: center; color: #000000; padding: ",[0,20]," 0; }\n.",[1],"up { color: rgb(255, 255, 255); font-size: ",[0,24],"; font-family: 微软雅黑; width: ",[0,200],"; height: ",[0,60],"; vertical-align: middle; text-align: center; line-height: ",[0,60],"; border-radius: ",[0,25],"; background-color: #3cc51f; }\n.",[1],"user-probabilitys { font-family: 微软雅黑; color: rgb(35,175,8); text-align: center; }\n.",[1],"bkinfo { margin: ",[0,30]," ",[0,40]," 0; display: -webkit-box; display: -webkit-flex; display: flex; -webkit-box-orient: horizontal; -webkit-box-direction: normal; -webkit-flex-direction: row; flex-direction: row; box-shadow: 0 ",[0,2]," ",[0,4]," ",[0,4]," #f2f2f2; padding: ",[0,10]," 0; border-bottom: ",[0,1]," solid #f2f2f2; }\n.",[1],"bkcard { display: -webkit-box; display: -webkit-flex; display: flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -webkit-flex-direction: column; flex-direction: column; color: #fff; text-align: center; color: #000000; }\n.",[1],"bkname { margin-bottom: ",[0,10],"; }\n.",[1],"bkcontent { overflow: hidden; text-overflow: ellipsis; -webkit-box-orient: vertical; -webkit-line-clamp: 2; font-size: ",[0,24],"; color: #858585; margin: 0 ",[0,40],"; line-height: ",[0,40],"; text-indent: ",[0,48],"; text-align: left; }\n.",[1],"bkform { font-size: ",[0,20],"; color: #c0c0c0; border-top: ",[0,1]," solid #eeeeee; margin: ",[0,30]," ",[0,40]," 0 ",[0,40],"; padding: ",[0,20],"; }\n",],undefined,{path:"./pages/nine/aipimg/index.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/nine/aipimg/index.wxml') } }));
+        }
+        if(window.__uniAppViewReady__){
+          uniAppViewReadyCallback()
+        }else{
+          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
+        }
+      })();
+      
